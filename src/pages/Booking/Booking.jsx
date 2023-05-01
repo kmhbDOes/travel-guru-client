94eb26b381ef1268d7fd63cf785668a6aa716a56
kmhbDOes <kmhbipul@gmail.com>
2023-05-02 01:03:00 +0600

    Functionality built overall

--- src/pages/Booking/Booking.jsx
+++ src/pages/Booking/Booking.jsx
@@ -4,13 +4,16 @@ import { useLoaderData } from "react-router-dom";
 const Booking = () => {
   const booking = useLoaderData();
   console.log(booking);
-  const { title, details } = booking;
+  const { title, details, photo } = booking;
   return (
     <div>
       <div>
         <div>
           <p>{title}</p>
           <p>{details}</p>
+          <p>
+            <img src={photo} alt="" />
+          </p>
         </div>
         <div>Booking</div>
       </div>
